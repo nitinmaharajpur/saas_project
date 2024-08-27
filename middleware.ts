43b53eadeb0a6d826d73e9120d7c378e3d63e191
Nitin Kumar <nitimaharajpur@gmail.com>
2024-08-28 01:26:45 +0530

    add cloudinary middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 
 const isPublicRoute=createRouteMatcher([
@@ -7,13 +8,37 @@ const isPublicRoute=createRouteMatcher([
     "/",
     "/home",
 ])
-export default clerkMiddleware()
+
+const isPublicApiRoute= createRouteMatcher([
+    "/api/videos"
+])
+
+
+export default clerkMiddleware((auth,req)=>{
+    const {userId}=auth();
+    const currentUrl=new URL(req.url)
+    const isAccessingDashboard=currentUrl.pathname==="/home"
+    const isApiRequest=currentUrl.pathname.startsWith("/api")
+//if they are logged in
+    if(userId && isPublicRoute(req) && !isAccessingDashboard){
+        return NextResponse.redirect(new URL("/home",req.url))
+    }
+    //if they are not logged in
+
+    if(!userId){
+        if(!isPublicRoute(req) && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/signin",req.url))
+        }
+
+        if(!isApiRequest && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/signin",req.url))
+        }
+    }
+    return NextResponse.next()
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!.*\\..*|_next).*","/","/(api|trpc)(.*)"
   ],
 }
